d7b74feac782ff22da309508fe7b3f8b8bcef2eb
angelesmarin <angeles.marinbatana@bsu.edu>
2024-10-28 21:56:45 -0400

    testing server

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("hello")
+/*
+this will be the entry point for the server  
+*/
+
+const express = require(`express`);
+const app = express()
+const port = 3000; 
+
+app.get(`/`, (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(port, () =>{
+    console.log(`port: ${port}`)
+})
