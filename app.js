eb62ef256e5a4a1e86597be6963d18b3b19358e6
angelesmarin <angeles.marinbatana@bsu.edu>
2024-10-29 08:19:58 -0400

    add basic file structure

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 /*
-this will be the entry point for the server  
+this is the server code
 */
 
-const express = require(`express`);
+//initialize express 
+const express = require(`express`); 
 const app = express()
-const port = 3000; 
+const port = 3000; //define port 
 
+//define route
 app.get(`/`, (req, res) => {
     res.send("Hello World")
 })
 
+//start server 
 app.listen(port, () =>{
     console.log(`port: ${port}`)
 })
