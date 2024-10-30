39ab4d948099153fbd4a695d008de80afd801e56
angelesmarin <angeles.marinbatana@bsu.edu>
2024-10-30 02:53:50 -0400

    update app.js

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 /*
-this is the server code
+this is the server to connect to postgresql database 
 */
 
-//initialize express 
-const express = require(`express`); 
-const app = express()
-const port = 3000; //define port 
+//initialize 
+const express = require(`express`);
+const { Pool } = require('pg'); //pool class will manage db connctions 
+require('dotenv').config(); 
+
+const app = express() //init exppress app 
+const port = process.env.PORT || 3000; //use port 3000
+
+//pg connection pool
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+
+//to handle JSON requests 
+app.use(express.json());
 
 //define route
-app.get(`/`, (req, res) => {
-    res.send("Hello World")
+pool.connect((error) => {
+    if (error) {
+        console.error('Trouble connecting to database :(', error);
+    } else {
+        console.log('Database connection successfull!');
+    }
 })
 
 //start server 
